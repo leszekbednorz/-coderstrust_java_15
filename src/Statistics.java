7bf5773b9d0f2e0fcc727ef603700f2a79045f53
Leszek Bednorz <leszek.bednorz@crossover.com>
2019-08-05 21:49:27 +0200

    Introduce javadoc comment for Statistic class

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,5 +1,10 @@
 public class Statistics {
 
+    /**
+     * Calculate average values for array of integers
+     * @param values
+     * @return
+     */
     public double getAverage(int[] values){
 
         int sum = 0;
@@ -11,6 +16,11 @@ public class Statistics {
         return (double)sum/(double)values.length;
     }
 
+    /**
+     * Calculate average values for floating point array
+     * @param values
+     * @return
+     */
     public double getAverage(double[] values){
         double sum = 0;
 
